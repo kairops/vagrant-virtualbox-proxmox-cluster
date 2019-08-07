ea319907ad94287324927e301ed727c594fb0e51
Pedro Amador <pedroamador.rodriguez@gmail.com>
2019-08-07 18:52:38 +0200

    Fix: Download the ubuntu template before use it

--- bin/lxc_play.sh
+++ bin/lxc_play.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for number in 1 2 3; do
-    #vagrant ssh node${number} -c "sudo pveam download local ubuntu-16.04-standard_16.04.5-1_amd64.tar.gz"
+    vagrant ssh node${number} -c "sudo pveam download local ubuntu-16.04-standard_16.04.5-1_amd64.tar.gz"
     vagrant ssh node${number} -c "sudo pct create 10${number} local:vztmpl/ubuntu-16.04-standard_16.04.5-1_amd64.tar.gz -net0 name=eth0,bridge=vmbr0,ip=10.12.12.10${number}/24,gw=10.12.12.${number} -memory 128 -hostname test10${number}"
 done
 
